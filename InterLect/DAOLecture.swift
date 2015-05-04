f3ed65a21b0ca2e5589becc6bdee4876b2a849cc
EduardoTT <Eduardo@MacBook-Pro-de-Eduardo.local>
2015-05-04 20:24:04 -0300

    começo do back end

--- InterLect/DAOLecture.swift
+++ InterLect/DAOLecture.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+
+class DAOLecture {
+    
+    func saveLecture(lecture:Lecture)->Bool {
+        //verificar se pode criar!
+        var ref = Firebase(url:"https://scorching-torch-3197.firebaseio.com/InterLect/Lectures")
+        var passwords = ["panelist": lecture.panelistPassword, "audience": lecture.audiencePassword]
+        var usersRef = ref.childByAppendingPath(lecture.name)
+        usersRef.setValue(passwords)
+        
+        return true
+    }
+    
+    func nameAvailable(name:String)->Bool {
+        return true
+    }
+    
+    func authenticate(name:String,password:String)->Bool {
+        return true
+    }
+    
+    func getLecture(name:String)->Lecture? {
+//        var path = "https://scorching-torch-3197.firebaseio.com/InterLect/Lectures/\(name)"
+//        var ref = Firebase(url:path)
+//        ref.observeEventType(.ChildAdded, withBlock: { snapshot in
+//            let panelistPassword = snapshot.value.objectForKey("panelist") as? String
+//            let audiencePassword = snapshot.value.objectForKey("audience") as? String
+//            println("panelist:\(panelistPassword) audience:\(audiencePassword)")
+//        })
+        return Lecture(name: "teste", panelistPassword: "teste", audiencePassword: "teste")
+    }
+    
+    func isPanelist(name:String,password:String)->Bool {
+        return true
+    }
+    
+    func getQuestions(name:String)->[String] {
+        return ["a","b","c","d","e"]
+    }
+}
--- InterLect/WatchViewCtrl.swift
+++ InterLect/WatchViewCtrl.swift
@@ -13,4 +13,41 @@ class WatchViewCtrl:UIViewController {
     
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var name: UITextField!
+    var isPanelist:Bool?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.password.resignFirstResponder()
+        self.name.resignFirstResponder()
+    }
+    
+    @IBAction func watchLecture(sender: AnyObject) {
+        var daoLecture = DAOLecture()
+        var lecture:Lecture
+        var isPanelist:Bool
+        if (self.password == nil || self.password.text == "" || self.name == nil || self.name.text == "") {
+            //todo:alert em vez de println
+            println("Please fill all the fields")
+            return
+        }
+        if (daoLecture.authenticate(self.name.text,password: self.password.text)) {
+            lecture = daoLecture.getLecture(self.name.text)!
+            self.isPanelist = daoLecture.isPanelist(self.name.text,password: self.password.text)
+        } else {
+            //todo: alert em vez de println
+            println("Wrong name or password")
+            return
+        }
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "segueFromWatch"
+        {
+            if let destinationVC = segue.destinationViewController as? QuestionsTableViewCtrl{
+                destinationVC.isPanelist = self.isPanelist
+                destinationVC.nameLecture = self.name.text
+            }
+        }
+    }
+
 }
